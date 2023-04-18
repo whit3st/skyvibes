0898982eb77cbde7dca4d1a7de25349b40a14eda
whit3st <alican15033@gmail.com>
2023-04-18 13:19:13 +0300

    font localed

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,9 @@ export default {
         'cust': '-4.51px 2.26px 0.75px rgba(0, 0, 0, 0.15)',
         
       },
-      fontFamily: {
-        jost: ['Jost', 'sans-serif'],
-      },
+      // fontFamily: {
+      //   jost: ['Jost', 'sans-serif'],
+      // },
       background: {
         root: 'linear-gradient(243.18deg, #A07FFB 0%, #FF82CD 85.24%)',
       }
