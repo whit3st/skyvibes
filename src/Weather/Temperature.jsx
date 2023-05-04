f7d144ef82a11c6b61054896067357497a19de13
whit3st <alican15033@gmail.com>
2023-05-05 02:50:27 +0300

    better text color

--- src/Weather/Temperature.jsx
+++ src/Weather/Temperature.jsx
@@ -1,29 +1,39 @@
-import React from 'react'
+import React from "react";
 import WeatherDate from "./WeatherDate";
-import Details from './Details';
-export default function Temperature({ temp, feelsLike, description, currentWeather }) {
-  return (
-    <div className='flex flex-col relative gap-2'>
-      <span className='flex gap-5'>
-       <h1 className='text-9xl font-extralight drop-shadow-cust'>{ temp }</h1>
-       <p className="text-2xl drop-shadow-cust">°C</p>
-      </span>
-      
-      <p className='text-md font-extralight drop-shadow-cust'>Feels like { feelsLike }°C</p>
-      <p className='text-md font-extralight drop-shadow-cust'>{description}</p>
-      <WeatherDate
-                        date={new Date(
-                            (currentWeather.dt + currentWeather.timezone) * 1000
-                        )
-                            .toUTCString()
-                            .slice(0, -18)}
-      />
-      <Details
-                        wind={Math.floor(currentWeather.wind.speed * 3.6)}
-                        humidity={currentWeather.main.humidity}
-                        pressure={currentWeather.main.pressure}
-                    />
-    </div>
-    
-  )
+import Details from "./Details";
+export default function Temperature({
+    temp,
+    feelsLike,
+    description,
+    currentWeather,
+}) {
+    return (
+        <div className="flex flex-col relative gap-2">
+            <span className="flex gap-5">
+                <h1 className="text-9xl font-extralight drop-shadow-cust">
+                    {temp}
+                </h1>
+                <p className="text-2xl drop-shadow-cust">°C</p>
+            </span>
+
+            <p className="text-md font-light drop-shadow-cust">
+                Feels like {feelsLike}°C
+            </p>
+            <p className="text-md font-light drop-shadow-cust">
+                {description}
+            </p>
+            <WeatherDate
+                date={new Date(
+                    (currentWeather.dt + currentWeather.timezone) * 1000
+                )
+                    .toUTCString()
+                    .slice(0, -18)}
+            />
+            <Details
+                wind={Math.floor(currentWeather.wind.speed * 3.6)}
+                humidity={currentWeather.main.humidity}
+                pressure={currentWeather.main.pressure}
+            />
+        </div>
+    );
 }
